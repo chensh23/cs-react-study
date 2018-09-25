009e1d1e13e4b28dd488231e7c57957b69b899f0
chenshuang <c_sjiayou@163.com>
2018-09-25 17:15:16 +0800

    测试card组件

--- src/pages/user/index.js
+++ src/pages/user/index.js
@@ -2,12 +2,39 @@ import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import {Card} from "antd"
 
+const tabList = [{
+    key: 'tab1',
+    tab: 'tab1',
+}, {
+    key: 'tab2',
+    tab: 'tab2',
+}];
+const contentList = {
+    tab1: <p>content1</p>,
+    tab2: <p>content2</p>,
+};
 
 class User extends Component{
+    state = {
+        key: 'tab1',
+        noTitleKey: 'app',
+    }
+    onTabChange = (key, type) => {
+        console.log(key, type);//tab2 key
+        this.setState({ [type]: key });
+    }
+
     render(){
         return(
-            <Card title={'test'}>
-                test
+            <Card
+                style={{ width: '100%' }}
+                title="Card title"
+                extra={<a href="#">More</a>}
+                tabList={tabList}
+                activeTabKey={this.state.key}
+                onTabChange={(key) => { this.onTabChange(key, 'key'); }}
+            >
+                {contentList[this.state.key]}
             </Card>
         )
     }
